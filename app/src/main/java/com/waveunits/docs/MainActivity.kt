101aee14bf36a7669b2d1381c91da84dc81a3859
Your Name <you@example.com>
2026-09-16 12:09:04 +0300

    Add verticalScroll import

--- app/src/main/java/com/waveunits/docs/MainActivity.kt
+++ app/src/main/java/com/waveunits/docs/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
